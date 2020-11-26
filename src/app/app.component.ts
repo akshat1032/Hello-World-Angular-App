72bd829357793326f1a8870fb9b0a3ae7cba70f2
akshat1032 <akshat.30oct@gmail.com>
2020-11-26 23:41:20 +0530

    Added : Launch website on clicking icon added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "../assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
 
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz";
+  }
+
+  onClick($event){
+    window.open(this.url,"_blank");
   }
 }
