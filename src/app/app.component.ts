3e8bb2256c5efaff29ea43eb9a1fc63f3ad4f5f2
akshat1032 <akshat.30oct@gmail.com>
2020-11-27 00:02:16 +0530

    Added : Display hello message with valid name added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "../assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
-  userName : String = "";
+  userName : string = "";
+  nameError : string = "";
 
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz";
@@ -17,5 +18,14 @@ export class AppComponent {
 
   onClick($event){
     window.open(this.url,"_blank");
+  }
+
+  onInput($event){
+    const nameRegex = RegExp('^[A-Z]{1}[a-z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError = "Name is incorrect!";
   }
 }
